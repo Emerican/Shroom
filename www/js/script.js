317f4a5f536f3ff699d48326877a546cbddd955f
Matīss Strautmanis <matiss.strautmanis@gmail.com>
2015-02-15 05:11:24 +0200

    Adding to db, fa,
    Add users, products,groups, font awsome iconas. moš vēl kautkas.

--- www/js/script.js
+++ www/js/script.js
@@ -11,12 +11,14 @@ jQuery(function()
   var current_section = "main";
   var last_section = current_section;
 
+
   var section_change = function(section_name)
   {
     current_section = section_name;
     navigation.trigger('change');
     jQuery('section').hide();
     jQuery("#"+section_name).show();
+
   };
 
   navigation.on('change',function()
@@ -25,6 +27,7 @@ jQuery(function()
     navigation.find('p').html( current_section );
 
   }).trigger('change');;
+
 
   buttons.on('click',function()
   {
@@ -78,4 +81,43 @@ jQuery(function()
     target.parents('section').trigger('change');
     return false;
   });
+
+  $("#new_client_group_btn").click( function()
+         {
+          var client_group_name = jQuery("#group_name").val();
+          Mints.client_groups.new({name:client_group_name});
+        }
+      );
+
+
+  $("#new_user_btn").click(function()
+      {
+        var client_name = jQuery("#name").val();
+        var client_surname = jQuery("#surname").val();
+        var client_phone = jQuery("#phone").val();
+        var client_email = jQuery("#email").val();
+
+        Mints.clients.new({card_id:null,name:client_name,surname:client_surname,client_group_id:null,phone:client_phone,email:client_email,postpay:true});
+      }
+  );
+
+  $("#new_product_btn").click(function()
+  {
+    var product_name = jQuery("#product_name").val();
+    var product_price = jQuery("#product_price").val();
+
+    Mints.products.new({product_group_id:null,name:product_name,price:product_price,description:"default"});
+
+  }
+  );
+
+  $("#new_product_group_btn").click(function()
+  {
+    var product_group_name = jQuery("#product_group_name").val();
+
+    Mints.product_groups.new({name:product_group_name});
+
+  }
+  );
+
 });
